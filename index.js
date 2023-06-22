b98672798659446dbe3b7c8001653df06d800db5
Shoaib Ahmed <shabiraja994@gmail.com>
2023-06-23 01:10:48 +0500

    fix issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const cors = require("cors");
 const PORT=process.env.PORT ||4000
 const path = require("path");
+
 app.listen(PORT,()=>console.log(`server is working on ${PORT}`))
 
 app.use(express.json());
